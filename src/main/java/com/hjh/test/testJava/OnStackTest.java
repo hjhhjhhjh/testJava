e8f01ec96255047adf82c42d777ec3badb940f62
hjhhjhhjh <hjhhjhhjh@live.com>
2020-06-24 00:35:25 +0800

    提交栈上分配.....

---
+++ src/main/java/com/hjh/test/testJava/OnStackTest.java
@@ -0,0 +1,73 @@
+package com.hjh.test.testJava;
+
+/**
+ * 栈上分配,逃逸分析(翻车了.....)
+ * VM Options:
+ * -Xmx1050M -Xms500M -XX:+DoEscapeAnalysis
+ * -XX:+PrintGC -XX:-UseTLAB
+ * -XX:+EliminateAllocations
+ *
+ *
+ * 启动
+ * java  -server -Xmx10m -Xms10m -XX:+DoEscapeAnalysis
+ * -XX:+PrintGC -XX:-UseTLAB -XX:+EliminateAllocations
+ * -classpath "D:\Study\test\testJava\target\classes" com.hjh.test.testJava.OnStackTest
+ *
+ *
+ */
+public class OnStackTest {
+    public static class User {
+        private int id=0;
+
+        private String name="";
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+//    public static void alloc() {
+//        User user = new User();
+//        user.id = 10;
+//        user.name = "aa";
+//    }
+//
+//    public static void main(String[] args) {
+//        long start = System.currentTimeMillis();
+//        for (int i = 0; i < 1000000000; i++) {
+//            alloc();
+//        }
+//        long end = System.currentTimeMillis();
+//
+//        System.out.println(end - start);
+//    }
+
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000000; i++) {
+            alloc();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    private static void alloc() {
+        User user = new User();
+        user.setId(1);
+        user.setName("blueStarWei");
+    }
+}
